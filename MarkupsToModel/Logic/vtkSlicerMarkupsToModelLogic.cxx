ee838d064e94bf447be729566ca14a758a9894f0
Thomas Vaughan <vaughan.thomas.a@gmail.com>
2017-10-02 12:28:02 -0400

    Separated implementation of helper namespaces from declaration.

--- MarkupsToModel/Logic/vtkSlicerMarkupsToModelLogic.cxx
+++ MarkupsToModel/Logic/vtkSlicerMarkupsToModelLogic.cxx
@@ -268,7 +268,7 @@ void vtkSlicerMarkupsToModelLogic::UpdateOutputModel(vtkMRMLMarkupsToModelNode* 
 }
 
 //------------------------------------------------------------------------------
-void vtkSlicerMarkupsToModelLogic::ProcessMRMLNodesEvents(vtkObject* caller, unsigned long event, void* callData)
+void vtkSlicerMarkupsToModelLogic::ProcessMRMLNodesEvents(vtkObject* caller, unsigned long event, void* /*callData*/ )
 {
   vtkMRMLNode* callerNode = vtkMRMLNode::SafeDownCast(caller);
   if (callerNode == NULL)
